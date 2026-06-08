b3014a5ce1a42aabc1fc90e83c94ad982968a7b7
Mishutka <mishkokorev35@gmail.com>
2026-06-08 17:44:34 +0300

    [v1.1]

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -4,23 +4,57 @@ import (
 	"WallGo/internal/handler"
 	"WallGo/internal/repository"
 	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/redis/go-redis/v9"
 )
 
+
 func main() {
+	redisHost := os.Getenv("REDIS_HOST")
+	if redisHost == "" {
+		redisHost = "localhost"
+	}
+	redisPort := os.Getenv("REDIS_PORT")
+	if redisPort == "" {
+		redisPort = "6379"
+	}
+
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     fmt.Sprintf("%s:%s", redisHost, redisPort),
 		Password: "",
 		DB:       0,
 	})
 
+	dbHost := os.Getenv("DB_HOST")
+	if dbHost == "" {
+		dbHost = "localhost"
+	}
+	dbPort := os.Getenv("DB_PORT")
+	if dbPort == "" {
+		dbPort = "5432"
+	}
+	dbUser := os.Getenv("DB_USER")
+	if dbUser == "" {
+		dbUser = "postgres"
+	}
+	dbPass := os.Getenv("DB_PASSWORD")
+	if dbPass == "" {
+		dbPass = "secret"
+	}
+	dbName := os.Getenv("DB_NAME")
+	if dbName == "" {
+		dbName = "pastebin_db"
+	}
+
 	ctx := context.Background()
-	dsn := "postgres://postgres:secret@localhost:5432/pastebin_db?sslmode=disable"
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", dbUser, dbPass, dbHost, dbPort, dbName)
+	
 	pgRepo, err := repository.NewPostgresql(ctx, dsn)
 	if err != nil {
 		log.Fatal(err)
@@ -38,3 +72,4 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
